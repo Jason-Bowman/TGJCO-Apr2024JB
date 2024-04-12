e9e48f6cc65906514f974b1d1cb5bda969d6972f
JBDigital21 <124791177+JBDigital21@users.noreply.github.com>
2024-04-12 11:45:44 +0100

    Updates

--- DiceRoll.java
+++ DiceRoll.java
@@ -1,8 +1,47 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class DiceRoll {
 
     // Static method to roll a die with a specified number of sides
     public static int roll(int sides) {
         // Implement dice rolling logic
     }
+
+    public static int[] rollAllDice(int[] diceTypes) {
+        // input [10, 10, 8, 8, 8]
+        // Then iterate through array and roll(num)
+        int numberOfDice = diceTypes.length;
+        int [] results = new int[numberOfDice];
+        for (int i = 0; i < numberOfDice; i++) {
+            int answer = roll(diceTypes[i]);
+            results[i] = answer;
+        }
+        return results;
+    }
+
+    public static enum Operator {
+        ADD,
+        SUBTRACT,
+        MULTIPLY
+    }
+
+    public static int executeMaths(int[] diceResults, Operator operator, int number) {
+        int totalOfDice = Arrays.stream(diceResults).sum();
+        switch (operator) {
+            case ADD -> {
+                return totalOfDice + number;
+            }
+            case SUBTRACT -> {
+                return totalOfDice - number;
+            }
+            case MULTIPLY -> {
+                return totalOfDice * number;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + operator);
+        }
+    }
+    
+
 
 }
--- WRITEUP.md
+++ WRITEUP.md
@@ -1,3 +1,34 @@
-Write details of what you have done for the judge's consideration here. 
+- Regex to parse input
+- Validation on input 
+- Logic for adding it together
+- Random number generator
+- Explanation
+- 
 
-This should include what features you have implemented, how they work, and why you think you should win.
+Sequence of events:
+
+1. Input is parsed and validated
+2. Relevant components are passed to the dice roller
+3. Random numbers are generated
+4. All added together with optional arithmetic options
+5. Result is compiled and printed to console
+
+
+void calculateResponse(String input) {
+    // regex parse
+
+    // rollSingularTypOfDie * number of times required
+
+
+    console.log(resultString)
+}
+
+
+Array[int] rollSingularTypeOfDie(int numberOfDice, int typeOfDie)
+
+int finalResult(Array[int] diceFaces, int mathsExpression)
+
+
+rollDie(int numberOfSides) {
+  // random number between 1 and numberOfSides
+}
